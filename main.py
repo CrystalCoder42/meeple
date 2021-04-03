75c8c36b4c3b1119b2ec6885982dcc7f1a30a2b2
Killashandra Scheuring <killashandra@sylvia.com>
2021-04-03 00:08:06 -0500

    Got pyqt5 to use pill images

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QIcon, QPixmap, QImage
+import sys
+import io
+from PIL import Image
 
 
-# Press the green button in the gutter to run the script.
+def pil2pixmap(image):
+    """
+    src: https://stackoverflow.com/questions/28086613/pillow-pil-to-qimage-conversion-python-exe-has-stopped-working
+    :param image: The pillow image object
+    :return: The QPixmap object
+    """
+    bytes_img = io.BytesIO()
+    image.save(bytes_img, format='JPEG')
+
+    qimg = QImage()
+    qimg.loadFromData(bytes_img.getvalue())
+
+    return QPixmap.fromImage(qimg)
+
+
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 image - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.run()
+
+    def run(self):
+        img = Image.new('RGB', (1000, 1000), color='black')
+        self.setWindowTitle(self.title)
+
+        # Create widget
+        label = QLabel(self)
+        label.setPixmap(pil2pixmap(img))
+        self.resize(img.width, img.height)
+
+        self.show()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
