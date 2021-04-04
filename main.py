1b765388667766501a0ed3dfe93e9adfe89f3598
Killashandra Scheuring <killashandra@sylvia.com>
2021-04-03 21:34:23 -0500

    got vector movement and frames working

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QIcon, QPixmap, QImage
 import sys
 import io
-from PIL import Image
+import time
+import random
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QIcon, QPixmap, QImage
+from PyQt5.QtCore import QTimer
+from PIL import Image, ImageDraw
 
 
 def pil2pixmap(image):
@@ -20,30 +23,86 @@ def pil2pixmap(image):
     return QPixmap.fromImage(qimg)
 
 
-class App(QWidget):
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+
+class Meeple:
+    def __init__(self, image, initial_position):
+        """
+        Initialize a meeple
+        :param Image image: The image to draw on
+        :param initial_position: The initial position
+        """
+        self.image = image
+        self.drawer = ImageDraw.Draw(self.image)
+        self.x = initial_position[0]
+        self.y = initial_position[1]
+        self.color = "blue"
+        self.size = 20
+        self.vector = Vector(10.5, 10.5)
+
+    def draw(self):
+        top_left = (self.x - (self.size/2), self.y - (self.size/2))
+        bottom_right = (self.x + (self.size/2), self.y + (self.size/2))
+        shape = [top_left, bottom_right]
+        self.drawer.rectangle(shape, self.color)
+
+    def update(self):
+        self.move((self.x + self.vector.x, self.y + self.vector.y))
+        if self.x > self.image.width:
+            self.move((0, self.y))
+        if self.x < 0:
+            self.move((self.image.width, self.y))
+        if self.y > self.image.height:
+            self.move((self.x, 0))
+        if self.y < 0:
+            self.move((self.x, self.image.height))
+
+    def move(self, new_position):
+        self.x, self.y = new_position
+
+
+class GameBoard(QWidget):
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 image - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
-        self.run()
+        self.width = 1000
+        self.height = 1000
 
-    def run(self):
-        img = Image.new('RGB', (1000, 1000), color='black')
-        self.setWindowTitle(self.title)
+        self.resize(self.width, self.height)
+        self.img = Image.new('RGB', (self.width, self.height), color='black')
+        self.drawer = ImageDraw.Draw(self.img)
+        self.meeple = Meeple(self.img, (self.width/2, self.height/2))
 
-        # Create widget
-        label = QLabel(self)
-        label.setPixmap(pil2pixmap(img))
-        self.resize(img.width, img.height)
+        self.label = QLabel(self)
 
+        self.timer = QTimer()
+        self.timer.start(100)
+        self.timer.timeout.connect(self.update)
+
+    def draw(self):
+        self.drawer.rectangle([(0, 0), (self.width, self.height)], "black")
+        self.meeple.draw()
+        self.label.setPixmap(pil2pixmap(self.img))
         self.show()
+
+    def update(self):
+        self.meeple.update()
+        self.meeple.vector.x += random.randint(-10, 10)
+        self.meeple.vector.y += random.randint(-10, 10)
+        self.draw()
+
+    def get_image(self):
+        return self.img
+
+    def get_pixel(self, x, y):
+        rgb_im = self.img.convert('RGB')
+        return rgb_im.getpixel((x, y))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    ex = GameBoard()
     sys.exit(app.exec_())
