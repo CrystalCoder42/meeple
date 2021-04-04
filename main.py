bbd24ae441d314db0ec2f59ea675a1473d03f793
Killashandra Scheuring <killashandra@sylvia.com>
2021-04-04 13:05:34 -0500

    updated meeple update. added requirements

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sys
 import io
-import time
 import random
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QIcon, QPixmap, QImage
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer
 from PIL import Image, ImageDraw
 
@@ -32,34 +31,37 @@ class Vector:
 class Meeple:
     def __init__(self, image, initial_position):
         """
-        Initialize a meeple
+        Initializes a meeple
         :param Image image: The image to draw on
-        :param initial_position: The initial position
+        :param initial_position: The initial position of the meeple
         """
-        self.image = image
-        self.drawer = ImageDraw.Draw(self.image)
-        self.x = initial_position[0]
-        self.y = initial_position[1]
-        self.color = "blue"
+        self.x, self.y = initial_position
+
         self.size = 20
-        self.vector = Vector(10.5, 10.5)
+        self.color = "blue"
+        self.image = image
+        self.drawer = ImageDraw.Draw(image)
+        self.vector = Vector(10, 10)
 
     def draw(self):
-        top_left = (self.x - (self.size/2), self.y - (self.size/2))
-        bottom_right = (self.x + (self.size/2), self.y + (self.size/2))
+        top_left = (self.x - self.size/2, self.y - self.size/2)
+        bottom_right = (self.x + self.size/2, self.y + self.size/2)
         shape = [top_left, bottom_right]
-        self.drawer.rectangle(shape, self.color)
+        self.drawer.rectangle(shape, fill=self.color)
 
     def update(self):
         self.move((self.x + self.vector.x, self.y + self.vector.y))
         if self.x > self.image.width:
-            self.move((0, self.y))
+            self.x = 0
         if self.x < 0:
-            self.move((self.image.width, self.y))
+            self.x = self.image.width
         if self.y > self.image.height:
-            self.move((self.x, 0))
+            self.y = 0
         if self.y < 0:
-            self.move((self.x, self.image.height))
+            self.y = self.image.height
+
+        self.vector.x += random.randint(-10, 10)
+        self.vector.y += random.randint(-10, 10)
 
     def move(self, new_position):
         self.x, self.y = new_position
@@ -70,39 +72,34 @@ class GameBoard(QWidget):
         super().__init__()
         self.width = 1000
         self.height = 1000
-
         self.resize(self.width, self.height)
-        self.img = Image.new('RGB', (self.width, self.height), color='black')
+
+        self.background = "black"
+
+        self.img = Image.new("RGB", (1000, 1000), self.background)
         self.drawer = ImageDraw.Draw(self.img)
-        self.meeple = Meeple(self.img, (self.width/2, self.height/2))
-
+        self.meeple = Meeple(self.img, (self.img.width / 2, self.img.height / 2))
         self.label = QLabel(self)
-
         self.timer = QTimer()
-        self.timer.start(100)
-        self.timer.timeout.connect(self.update)
+        self.run()
 
     def draw(self):
-        self.drawer.rectangle([(0, 0), (self.width, self.height)], "black")
+        self.drawer.rectangle([(0, 0), (self.width, self.height)], self.background)
         self.meeple.draw()
         self.label.setPixmap(pil2pixmap(self.img))
-        self.show()
 
     def update(self):
         self.meeple.update()
-        self.meeple.vector.x += random.randint(-10, 10)
-        self.meeple.vector.y += random.randint(-10, 10)
         self.draw()
 
-    def get_image(self):
-        return self.img
+    def run(self):
+        self.draw()
+        self.show()
+        self.timer.start(100)
+        self.timer.timeout.connect(self.update)
 
-    def get_pixel(self, x, y):
-        rgb_im = self.img.convert('RGB')
-        return rgb_im.getpixel((x, y))
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = GameBoard()
     sys.exit(app.exec_())
