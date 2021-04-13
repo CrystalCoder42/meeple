053da02c63959498e1eeeadc5000b9f04a5b05f0
Killashandra Scheuring <killashandra@sylvia.com>
2021-04-12 19:21:46 -0500

    Matching recording

--- main.py
+++ main.py
@@ -23,102 +23,83 @@ def pil2pixmap(image):
     return QPixmap.fromImage(qimg)
 
 
-def get_arc_positions(degrees, radius, center, current_pos=(0, 0)):
-    return [
-        (np.cos(np.deg2rad(angle)) * radius + current_pos[0], np.sin(np.deg2rad(angle)) * radius + current_pos[1])
-        for angle in range(int(center - degrees / 2), int(center + degrees / 2))
-    ]
-
-
-def get_pos_in_arc(degree, radius, current_pos=(0, 0)):
-    return np.cos(np.deg2rad(degree)) * radius + current_pos[0], np.sin(np.deg2rad(degree)) * radius + current_pos[1]
-
-
-def get_radius_positions(degree, max_radius, current_pos=(0, 0)):
-    return [
-        (np.cos(np.deg2rad(degree)) * radius + current_pos[0], np.sin(np.deg2rad(degree)) * radius + current_pos[1])
-        for radius in range(max_radius)
-    ]
+def get_arc_xy(angle, radius, center=(0, 0)):
+    return np.cos(np.deg2rad(angle)) * radius + center[0], np.sin(np.deg2rad(angle)) * radius + center[1]
 
 
 class Vector:
     def __init__(self, direction, magnitude):
-        self.direction = direction
+        self.direction = direction  # Degrees
         self.magnitude = magnitude
 
-    def get_new_position(self, current_position):
-        x, y = current_position
+    def new_position(self, initial_position):
+        x, y = initial_position
         x += np.cos(np.deg2rad(self.direction)) * self.magnitude
         y += np.sin(np.deg2rad(self.direction)) * self.magnitude
         return x, y
+
+
+class Sensor:
+    def __init__(self, angle_range, radius_range):
+        self.angle = angle_range
+        self.radius = radius_range
+
+    def angle_range(self, center):
+        return range(int(center - self.angle / 2), int(center + self.angle / 2))
 
 
 class Meeple:
     def __init__(self, parent, initial_position, color="blue"):
         """
         Initializes a meeple
-        :param GameBoard parent: The parent game board
-        :param tuple initial_position: The initial position of the meeple
+        :param GameBoard parent: The game board to draw on
+        :param initial_position: The initial position of the meeple
         """
         self.x, self.y = initial_position
 
         self.size = 20
-        self.angle = 100
-        self.sensor_radius = 30
         self.color = color
         self.parent = parent
-        self.vector = Vector(-30, 20)
+        self.vector = Vector(random.randint(0, 360), random.randint(10, 30))
+        self.sensor = Sensor(90, self.size * 2)
 
     def draw(self):
-        shape = [(self.x - self.size / 2, self.y - self.size / 2), (self.x + self.size / 2, self.y + self.size / 2)]
+        top_left = (self.x - self.size / 2, self.y - self.size / 2)
+        bottom_right = (self.x + self.size / 2, self.y + self.size / 2)
+        shape = [top_left, bottom_right]
         self.parent.drawer.ellipse(shape, fill=self.color)
         # shape = [
-        #     (self.x - self.sensor_radius, self.y - self.sensor_radius),
-        #     (self.x + self.sensor_radius, self.y + self.sensor_radius)
+        #     (self.x - self.sensor.radius, self.y - self.sensor.radius),
+        #     (self.x + self.sensor.radius, self.y + self.sensor.radius)
         # ]
         # self.parent.drawer.arc(
         #     shape,
-        #     int(self.vector.direction - self.angle / 2),
-        #     int(self.vector.direction + self.angle / 2),
+        #     int(self.vector.direction - self.sensor.angle / 2),
+        #     int(self.vector.direction + self.sensor.angle / 2),
         #     "red"
         # )
 
     def update(self):
-        try:
-            self.sense()
-        except:
-            print(sys.exc_info())
-        self.move(self.vector.get_new_position((self.x, self.y)))
+        self.sense()
+        self.move(self.vector.new_position((self.x, self.y)))
 
     def move(self, new_position):
         self.x, self.y = new_position
         self.x = min(max(0, self.x), self.parent.image.width)
         self.y = min(max(0, self.y), self.parent.image.height)
 
-    def position(self):
-        return self.x, self.y
-
-    def sensor_arc_range(self):
-        return range(int(self.vector.direction - self.angle / 2),
-                     int(self.vector.direction + self.angle / 2))
-
     def sense(self):
-        """
-        Draws the pie shape of the meeples sensor
-        """
-        for radius in range(1, self.sensor_radius + 1):
-            for angle in self.sensor_arc_range():
+        for radius in range(1, self.sensor.radius + 1):
+            for angle in self.sensor.angle_range(self.vector.direction):
                 try:
-                    point_color = self.parent.background.get_pixel(
-                        get_pos_in_arc(angle, self.sensor_radius, self.position())
-                    )
+                    point_color = self.parent.background.get_pixel(get_arc_xy(angle, radius, (self.x, self.y)))
                 except:
                     point_color = (0, 0, 0)
-                if point_color in self.parent.background.obstacle_colors.values():
-                    starting_angle = int(self.vector.direction - self.angle / 2)
+                if point_color == (0, 0, 0):
+                    starting_angle = int(self.vector.direction - self.sensor.angle / 2)
                     while starting_angle < 0:
                         starting_angle += 360
-                    min_turn = min(int(180 * ((radius + 2)/self.sensor_radius)), 180)
+                    min_turn = min(int(180 * ((radius + 2) / self.sensor.radius)), 180)
                     turn_range = (360 - starting_angle + min_turn, starting_angle + min_turn)
                     change_by = random.randint(min(*turn_range), max(*turn_range))
                     self.vector.direction += change_by
@@ -132,19 +113,26 @@ class Meeple:
 
 class Background:
     def __init__(self, wh, color="white", stroke="black"):
-        self.image = Image.new("RGB", wh)
+        self.image = Image.new("RGB", wh, color)
         self.drawer = ImageDraw.Draw(self.image)
-        self.drawer.rectangle([(0, 0), wh], color, stroke, width=10)
+        self.drawer.rectangle([(0, 0), wh], color, stroke, 10)
         self.drawer.ellipse(
             [(100, 100), (300, 300)],
             "black"
         )
-        self.obstacle_colors = {
-            "border": (0, 0, 0)
-        }
+        for _ in range(random.randint(10, 30)):
+            x, y = self.random_point()
+            size = random.randint(50, 100)
+            self.drawer.ellipse(
+                [x - size/2, y-size/2, x + size/2, y+size/2],
+                (0, 0, 0)
+            )
 
-    def get_pixel(self, position):
-        return self.image.getpixel(position)
+    def get_pixel(self, xy):
+        return self.image.getpixel(xy)
+
+    def random_point(self):
+        return random.randint(0, self.image.width), random.randint(0, self.image.height)
 
 
 class GameBoard(QWidget):
@@ -154,13 +142,14 @@ class GameBoard(QWidget):
         self.height = 1000
         self.resize(self.width, self.height)
 
-        self.background = Background((1000, 1000))
+        self.background = Background((self.width, self.height))
 
         self.image = Image.new("RGB", (1000, 1000))
         self.drawer = ImageDraw.Draw(self.image)
         self.meeples = [
-            Meeple(self, self.random_point(), "blue"),
-            Meeple(self, self.random_point(), "red"),
+            Meeple(self, self.random_point()),
+            # Meeple(self, self.random_point(), "yellow"),
+            # Meeple(self, self.random_point(), "red"),
             Meeple(self, self.random_point(), "green"),
         ]
         self.label = QLabel(self)
@@ -168,7 +157,7 @@ class GameBoard(QWidget):
         self.run()
 
     def random_point(self):
-        return random.randint(20, self.width - 20), random.randint(20, self.height - 20)
+        return random.randint(0, self.width), random.randint(0, self.height)
 
     def draw(self):
         self.image.paste(self.background.image)
